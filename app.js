b2df0b53692d05dd224d5d340d0c1ac4d2d9c41f
shuai <zhangs@xinguangnet.com>
2018-01-08 14:34:17 +0800

    聊天室

---
+++ app.js
@@ -0,0 +1,78 @@
+const Express = require('express');
+const path = require('path');
+
+const app = new Express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const port = process.env.PORT || 3000;
+
+app.use(Express.static(path.join(__dirname, 'public')));
+app.set('views', __dirname + '/views');
+
+io.serveClient('log level', 1);
+
+io.on('connection', socket => {
+    let client;
+
+    socket.emit('open');
+    console.log(socket.handshake);
+
+    client = {
+        socket: socket,
+        name: false,
+        color: getColor(),
+    };
+
+    socket.on('message', msg => {
+        let obj = { time: getTime(), color: client.color };
+
+        if (!client.name) {
+            client.name = msg;
+            obj['text'] = client.name;
+            obj['author'] = 'System';
+            obj['type'] = 'welcome';
+            console.log(client.name + 'login');
+
+            socket.emit('system', obj);
+        } else {
+            obj['text'] = msg;
+            obj['author'] = client.name;
+            obj['type'] = 'message';
+            console.log(client.name + 'say: ' + msg);
+
+            socket.emit('message', obj);
+            socket.broadcast.emit('message', obj);
+        }
+    });
+
+    socket.on('disconnect', () => {
+        let obj = {
+            time: getTime(),
+            color: client.color,
+            author: 'System',
+            text: client.name,
+            type: 'disconnect'
+        };
+        socket.broadcast.emit('system', obj);
+        console.log(client.name + ' Disconnect');
+    });
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'views','chat.html'));
+});
+
+server.listen(port,function(){
+    console.log('server listening at port %d',port);
+});
+
+let getTime = () => {
+    let date = new Date();
+    return date.getHours() + ":" + date.getMinutes() + date.getSeconds();
+}
+
+let getColor = () => {
+    let colors = ['aliceblue', 'antiquewhite', 'aqua', 'aquamarine', 'pink', 'red', 'green',
+        'orange', 'blue', 'blueviolet', 'brown', 'burlywood', 'cadetblue'];
+    return colors[Math.round(Math.random() * 10000 % colors.length)];
+}
---
+++ public/javascripts/chat.js
@@ -0,0 +1,43 @@
+$(function(){
+    let content = $('#content');
+    let status = $('#status');
+    let input = $('#input');
+    let myName = false;
+
+    socket = io.connect('http://localhost:3000');
+    socket.on('open',function(){
+        status.text('choose a name');
+    });
+
+    socket.on('system',function(json){
+        let p = '';
+        if(json.type === 'welcome'){
+            if(myName==json.text){
+                status.text(myName +': ').css('color',json.color);
+                p = '<p style="background:' +json.color+'">system@ '+json.time+' :welcome '+json.text+'</p>';
+            }else if(json.type=='disconnect'){
+                p = '<p style="background:'+json.color +'">system@ '+json.time+' :Bye'+json.text+'</p>';
+            }
+            content.prepend(p);
+        }
+    })
+
+    socket.on('message',function(json){
+        let p = '<p><span style="color:'+json.color+';">'+json.author+'</span> @' +json.time +' :' +json.text+'</p>';
+        content.prepend(p);
+    });
+
+    input.keydown(function(e){
+        if(e.keyCode === 13){
+            let msg = $(this).val();
+            if(!msg) return;
+
+            socket.send(msg);
+
+            $(this).val('');
+            if(myName ===false){
+                myName = msg;
+            }
+        }
+    })
+});
